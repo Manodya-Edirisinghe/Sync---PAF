e4f85e33d9dfbcc34ffe2bde7dcfcbb21177fc20
llithira3 <llithira3@gmail.com>
2026-04-01 22:53:08 +0530

    feat: add UI components and backend application test suite

--- backend/src/test/java/com/smartcampus/operationshub/OperationsHubApplicationTests.java
+++ backend/src/test/java/com/smartcampus/operationshub/OperationsHubApplicationTests.java
@@ -3,7 +3,18 @@ package com.smartcampus.operationshub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.security.oauth2.client.registration.google.client-id=test-client",
+    "spring.security.oauth2.client.registration.google.client-secret=test-secret"
+})
 class OperationsHubApplicationTests {
 
     @Test
